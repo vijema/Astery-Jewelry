da7c0728a8daf2f0736e934287b915512175179d
VIJEMA <vko.projects@gmail.com>
2023-08-16 17:58:23 +0500

    Sales infor restore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import "../styles/globals.css";
 import { Navbar } from "../components/Navbar";
 
 
+
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
@@ -14,6 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
       <ChakraProvider>
+        
         <Navbar />
         <Component {...pageProps} />
       </ChakraProvider>
